452c2b7fab3b25aabd60d35bd82dc573d08d4371
jhayfix <jhayfhix05@gmail.com>
2022-01-29 15:09:33 -0800

    Added About Page

--- js/main.js
+++ js/main.js
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
+toTop.style.display = 'none';
 
 function scrollFunction() {
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
